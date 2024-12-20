3f4247bcece79ae576f45dae71fe6ce58689aafa
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2024-12-20 15:58:38 -0700

    Updated cli of SWAPI for new science dependencies

--- imap_processing/cdf/utils.py
+++ imap_processing/cdf/utils.py
@@ -128,9 +128,17 @@ def write_cdf(
     dataset.attrs["ground_software_version"] = imap_processing._version.__version__
     # Add any parent files to the dataset attributes
     if parent_files:
-        # Include the current files if there are any and include just the filename
-        # [file1.txt, file2.cdf, ...]
-        dataset.attrs["Parents"] = dataset.attrs.get("Parents", []) + [
+        # Get the current value of "Parents" and ensure it is a list
+        existing_parents = dataset.attrs.get("Parents", [])
+        if isinstance(existing_parents, str):
+            print("Parent in string format ", existing_parents)
+            print("Parent in string format ", existing_parents)
+            existing_parents = [existing_parents]  # Convert string to a list
+        elif not isinstance(existing_parents, list):
+            raise TypeError(f"Unexpected type for Parents: {type(existing_parents)}")
+
+        # Append new parent filenames
+        dataset.attrs["Parents"] = existing_parents + [
             parent_file.name for parent_file in parent_files
         ]
 
