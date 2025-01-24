335899acee07f7917e6e85a34ea7f2754054726b
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-01-24 14:14:43 -0700

    test fix

--- imap_processing/tests/swe/test_swe_l1b_science.py
+++ imap_processing/tests/swe/test_swe_l1b_science.py
@@ -5,10 +5,8 @@ import pandas as pd
 import pytest
 
 from imap_processing.swe.l1a.swe_science import swe_science
-from imap_processing.swe.l1b.swe_l1b import swe_l1b
 from imap_processing.swe.l1b.swe_l1b_science import (
     apply_in_flight_calibration,
-    calculate_calibration_factor,
     get_indices_of_full_cycles,
 )
 
@@ -55,35 +53,25 @@ def test_get_full_cycle_data_indices():
 )
 def test_in_flight_calibration_factor(mock_read_in_flight_cal_data, l1a_test_data):
     """Test that the L1B processing is working as expected."""
-    input_time = l1a_test_data["shcoarse"].data[0]  # 453051308
+    # create sample data
 
-    cal_factor = calculate_calibration_factor(input_time)
+    input_time = 453051355.0
+    input_count = 19967
+    one_full_cycle_data = np.full((24, 30, 7), input_count)
+    acquisition_time = np.full((24, 30, 7), input_time)
+
     # Test that calibration factor is within correct range given test data
-    expected_cal_factor = 1 + (2 - 1) / (453051900 - 453051300) * (
+    expected_cal_factor = 1 + ((2 - 1) / (453051900 - 453051300)) * (
         input_time - 453051300
     )
-    np.testing.assert_array_equal(cal_factor, np.repeat(expected_cal_factor, 7))
-    assert cal_factor.shape == (7,)
 
-    # Test that applying calibration factor works as expected.
-    # Picking non-zero data to test
-    # counts_data = l1a_test_data["science_data"].data[15]  # 19967
-    # l1b_data = apply_in_flight_calibration(counts_data, input_time)
-    # assert l1b_data.shape == (180, 7)
-    # Since data is zero in some index and 19967 in some index,
-    # we can't just multiply by expected_cal_factor to get expected result.
-    # Instead, we will just check that the first index is correct.
-    # assert l1b_data[0][1] == 19967 * expected_cal_factor
-
-    one_full_cycle_data = np.full((24, 30, 7), 19967)
-    # print(one_full_cycle_data)
-    acquisition_time = np.full((24, 30, 7), 453051355.0)
-    # print(acquisition_time)
-    apply_in_flight_calibration(
+    corrected_count = apply_in_flight_calibration(
         one_full_cycle_data,
         acquisition_time,
     )
-    # np.testing.assert_array_equal(cal_factor,
-    # np.full((24, 30, 7), 19967 * 1.09166667))
-    # process to l1b
-    swe_l1b(l1a_test_data, "001")
+
+    np.testing.assert_allclose(
+        corrected_count,
+        np.full((24, 30, 7), input_count * expected_cal_factor),
+        rtol=1e-9,
+    )
