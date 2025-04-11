ea4647eeda23a639e7530a014728c4a620f1a417
Greg Lucas <greg.m.lucas@gmail.com>
2025-04-11 07:02:34 -0600

    MNT: Factor out the Data_version logic into the common post-processing (#1593)
    The injection of the data version can happen at the CLI call site
    rather than having to be passed through to every single instrument.
    This means that an instrument doesn't need to specify a version
    to produce a product they can just produce a product and the
    infrastructure can decide which version that should be.
    
    There is a default version of v999 used when writing a file to cdf
    so that tests will produce valid filenames.

--- imap_processing/tests/swapi/test_swapi_l2.py
+++ imap_processing/tests/swapi/test_swapi_l2.py
@@ -9,21 +9,21 @@ def test_swapi_l2_cdf(swapi_l0_test_data_path):
     """Test housekeeping processing and CDF file creation"""
     test_packet_file = swapi_l0_test_data_path / "imap_swapi_l0_raw_20240924_v001.pkts"
     # Create HK CDF File
-    processed_hk_data = swapi_l1([test_packet_file], data_version="v001")
-    hk_cdf_filename = "imap_swapi_l1_hk_20240924_v001.cdf"
+    processed_hk_data = swapi_l1([test_packet_file])
+    hk_cdf_filename = "imap_swapi_l1_hk_20240924_v999.cdf"
     hk_cdf_path = write_cdf(processed_hk_data[0])
     assert hk_cdf_path.name == hk_cdf_filename
 
     # Create L1 CDF File
-    processed_sci_data = swapi_l1([test_packet_file, hk_cdf_path], data_version="v001")
-    cdf_filename = "imap_swapi_l1_sci_20240924_v001.cdf"
+    processed_sci_data = swapi_l1([test_packet_file, hk_cdf_path])
+    cdf_filename = "imap_swapi_l1_sci_20240924_v999.cdf"
     cdf_path = write_cdf(processed_sci_data[0])
     assert cdf_path.name == cdf_filename
 
     l1_dataset = processed_sci_data[0]
-    l2_dataset = swapi_l2(l1_dataset, data_version="v001")
+    l2_dataset = swapi_l2(l1_dataset)
     l2_cdf = write_cdf(l2_dataset)
-    assert l2_cdf.name == "imap_swapi_l2_sci_20240924_v001.cdf"
+    assert l2_cdf.name == "imap_swapi_l2_sci_20240924_v999.cdf"
 
     # Test uncertainty variables are as expected
     np.testing.assert_array_equal(
