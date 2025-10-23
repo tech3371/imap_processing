50566f42759b6f38c44db12d8812c4ad117bef79
Tenzin Choedon <tenzin.choedon@lasp.colorado.edu>
2025-10-23 09:49:58 -0600

    refactor to l1b work

--- imap_processing/codice/codice_l1b.py
+++ imap_processing/codice/codice_l1b.py
@@ -15,20 +15,15 @@ from pathlib import Path
 import numpy as np
 import xarray as xr
 
-from imap_processing import imap_module_directory
 from imap_processing.cdf.imap_cdf_manager import ImapCdfAttributes
 from imap_processing.cdf.utils import load_cdf
 from imap_processing.codice import constants
-from imap_processing.codice.utils import CODICEAPID
-from imap_processing.utils import packet_file_to_datasets
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def convert_to_rates(
-    dataset: xr.Dataset, descriptor: str, variable_name: str
-) -> np.ndarray:
+def convert_to_rates(dataset: xr.Dataset, descriptor: str) -> np.ndarray:
     """
     Apply a conversion from counts to rates.
 
@@ -41,14 +36,18 @@ def convert_to_rates(
         The L1b dataset containing the data to convert.
     descriptor : str
         The descriptor of the data product of interest.
-    variable_name : str
-        The variable name to apply the conversion to.
 
     Returns
     -------
     rates_data : np.ndarray
         The converted data array.
     """
+    # Variables to convert based on descriptor
+    variables_to_convert = getattr(
+        constants, f"{descriptor.upper().replace('-', '_')}_VARIABLE_NAMES"
+    )
+
+    # Now solve denominator based on data product type
     if descriptor in [
         "lo-counters-aggregated",
         "lo-counters-singles",
@@ -58,15 +57,10 @@ def convert_to_rates(
         "lo-sw-priority",
         "lo-ialirt",
     ]:
-        # Applying rate calculation described in section 10.2 of the algorithm
-        # document
-        # In order to divide by acquisition times, we must reshape the acq
-        # time data array to match the data variable shape
-        dims = [1] * dataset[variable_name].data.ndim
-        dims[1] = 128
-        acq_times = dataset.acquisition_time_per_step.data.reshape(dims)  # (128)
-        # Now perform the calculation
-        rates_data = dataset[variable_name].data / (
+        # Solve denominator, Acquisition times, of above products. Acquisition times
+        # must reshape to match the data variable shape
+        acq_times = dataset.acquisition_time_per_step.data[np.newaxis, :, np.newaxis]
+        denominator = (
             acq_times
             * 1e-3  # Converting from milliseconds to seconds
             * constants.L1B_DATA_PRODUCT_CONFIGURATIONS[descriptor]["num_spin_sectors"]
@@ -75,24 +69,27 @@ def convert_to_rates(
         "lo-nsw-species",
         "lo-sw-species",
     ]:
-        # Applying rate calculation described in section 10.2 of the algorithm
-        # document
-        # In order to divide by acquisition times, we must reshape the acq
-        # time data array to match the data variable shape (epoch, esa_step, sector)
-        dims = [1] * dataset[variable_name].data.ndim
-        dims[1] = 128
-        acq_times = dataset.acquisition_time_per_step.data.reshape(dims)  # (128)
+        # Solve denominator, Acquisition times, of above products. Acquisition times
+        # must reshape to match the data variable shape, (epoch, esa_step, sector)
+        acq_times = dataset.acquisition_time_per_step.data[np.newaxis, :, np.newaxis]
         # acquisition time have an array of shape (128,). We match n_sector to that.
         # Per CoDICE, fill first 127 with default value of 12. Then fill last with 11.
         n_sector = np.full(128, 12, dtype=int)
         n_sector[-1] = 11
 
-        # Now perform the calculation
-        rates_data = dataset[variable_name].data / (
+        # Convert counts to rates
+        denominator = (
             acq_times
             * 1e-3  # Converting from milliseconds to seconds
             * n_sector[:, np.newaxis]  # Spin sectors
         )
+
+        # Clean up attributes added by cdflib that are not needed
+        for variable in ["k_factor", "voltage_table", "acquisition_time_per_step"]:
+            # remove new DEPEND_1 added by cdflib
+            # TODO: revisit this after discussion with Bryan Harter
+            if "DEPEND_1" in dataset[variable].attrs:
+                del dataset[variable].attrs["DEPEND_1"]
     elif descriptor in [
         "hi-counters-aggregated",
         "hi-counters-singles",
@@ -101,15 +98,20 @@ def convert_to_rates(
         "hi-sectored",
         "hi-ialirt",
     ]:
-        # Applying rate calculation described in section 10.1 of the algorithm
-        # document
-        rates_data = dataset[variable_name].data / (
+        denominator = (
             constants.L1B_DATA_PRODUCT_CONFIGURATIONS[descriptor]["num_spin_sectors"]
             * constants.L1B_DATA_PRODUCT_CONFIGURATIONS[descriptor]["num_spins"]
             * constants.HI_ACQUISITION_TIME
         )
 
-    return rates_data
+    # For each variable, convert counts and uncertainty to rates
+    for variable in variables_to_convert:
+        dataset[variable].data = dataset[variable].data / denominator
+        # Uncertainty calculation
+        unc_variable = f"unc_{variable}"
+        dataset[unc_variable].data = dataset[unc_variable].data / denominator
+
+    return dataset
 
 
 def process_codice_l1b(file_path: Path) -> xr.Dataset:
@@ -150,27 +152,8 @@ def process_codice_l1b(file_path: Path) -> xr.Dataset:
     # Update the global attributes
     l1b_dataset.attrs = cdf_attrs.get_global_attributes(dataset_name)
     print(l1b_dataset.coords)
-    # This construct Lookup names from constants.py
-    # Eg HI_OMNI_VARIABLE_NAMES
-    variables_to_convert = getattr(
-            constants, f"{descriptor.upper().replace('-', '_')}_VARIABLE_NAMES"
-        )
-    print(variables_to_convert)
-    for variable_name in variables_to_convert:
-        # Convert counts to rates
-        l1b_dataset[variable_name].data = convert_to_rates(
-            l1b_dataset, descriptor, variable_name
-        )
-        # Convert uncertainties from counts to rates
-        unc_variable_name = f"unc_{variable_name}"
-        l1b_dataset[unc_variable_name].data = convert_to_rates(
-            l1b_dataset, descriptor, unc_variable_name
-        )
-
-    for var in l1b_dataset.data_vars:
-        print(l1b_dataset[var].attrs)
-    for var in l1b_dataset.coords:
-        print(l1b_dataset[var].attrs)
-    # print(l1b_dataset.coords)
-    # print(l1b_dataset.data_vars)
+    l1b_dataset = convert_to_rates(
+        l1b_dataset,
+        descriptor,
+    )
     return l1b_dataset
